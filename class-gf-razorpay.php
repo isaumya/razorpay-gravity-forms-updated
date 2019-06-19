da4b99d2c3ddfdc1792e3960baa9e4ce38367b32
Saumya Majumder <saumya0305@gmail.com>
2019-06-19 17:06:05 +0530

    Uploaded the project

---
+++ class-gf-razorpay.php
@@ -0,0 +1,463 @@
+<?php
+// Plugin path
+$dir = dirname(__DIR__);
+// Include the Razorpay PHP SDK
+require_once("{$dir}/razorpay-gravity-forms/razorpay-sdk/Razorpay.php");
+
+use Razorpay\Api\Api;
+use Razorpay\Api\Errors\SignatureVerificationError;
+
+GFForms::include_payment_addon_framework();
+
+// Start of the payment class
+class GFRazorpay extends GFPaymentAddOn {
+  //Razorpay plugin config key ID and key secret
+  const GF_RAZORPAY_KEY = 'gf_razorpay_key';
+  const GF_RAZORPAY_SECRET = 'gf_razorpay_secret';
+
+  //Razorpay API attributes
+  const RAZORPAY_ORDER_ID = 'razorpay_order_id';
+  const RAZORPAY_PAYMENT_ID = 'razorpay_payment_id';
+  const RAZORPAY_SIGNATURE = 'razorpay_signature';
+
+  //Cookie set for one day
+  const COOKIE_DURATION = 86400;
+
+  // Customer related fields
+  const CUSTOMER_FIELDS_NAME = 'name';
+  const CUSTOMER_FIELDS_EMAIL = 'email';
+  const CUSTOMER_FIELDS_CONTACT = 'contact';
+
+  // Declaring variables
+  protected $_version = GF_RAZORPAY_VERSION;
+  protected $_min_gravityforms_version = '1.9.3';
+  protected $_slug = 'razorpay-gravity-forms';
+  protected $_path = 'razorpay-gravity-forms/gf-razorpay.php';
+  protected $_full_path = __FILE__;
+  protected $_url = 'http://www.gravityforms.com';
+	protected $_title = 'Gravity Forms Razorpay Add-On';
+  protected $_short_title = 'Razorpay';
+  protected $_supports_callbacks = true;
+
+  // Permissions
+	protected $_capabilities_settings_page = 'gravityforms_razorpay';
+	protected $_capabilities_form_settings = 'gravityforms_razorpay';
+  protected $_capabilities_uninstall = 'gravityforms_razorpay_uninstall';
+  
+  // Automatic upgrade enabled
+  protected $_enable_rg_autoupgrade = false;
+  
+  private static $_instance = null;
+
+  public static function get_instance() {
+		if ( self::$_instance == null ) {
+			self::$_instance = new GFRazorpay();
+		}
+
+		return self::$_instance;
+  }
+  
+  private function __clone() {
+  } /* do nothing */
+  
+  public function init_frontend() {
+		parent::init_frontend();
+
+    add_filter( 'gform_disable_post_creation', array( $this, 'delay_post' ), 10, 3 );
+		add_filter( 'gform_disable_notification', array( $this, 'delay_notification' ), 10, 4 );
+  }
+
+  public function delay_post( $is_disabled, $form, $entry ) {
+
+		$feed            = $this->get_payment_feed( $entry );
+		$submission_data = $this->get_submission_data( $feed, $form, $entry );
+
+		if ( ! $feed || empty( $submission_data['payment_amount'] ) ) {
+			return $is_disabled;
+		}
+
+		return ! rgempty( 'delayPost', $feed['meta'] );
+	}
+
+	public function delay_notification( $is_disabled, $notification, $form, $entry ) {
+		if ( rgar( $notification, 'event' ) != 'form_submission' ) {
+			return $is_disabled;
+		}
+
+		$feed            = $this->get_payment_feed( $entry );
+		$submission_data = $this->get_submission_data( $feed, $form, $entry );
+
+		if ( ! $feed || empty( $submission_data['payment_amount'] ) ) {
+			return $is_disabled;
+		}
+
+		$selected_notifications = is_array( rgar( $feed['meta'], 'selectedNotifications' ) ) ? rgar( $feed['meta'], 'selectedNotifications' ) : array();
+
+		return isset( $feed['meta']['delayNotification'] ) && in_array( $notification['id'], $selected_notifications ) ? true : $is_disabled;
+	}
+  
+  //----- SETTINGS PAGES ----------//
+  public function plugin_settings_fields(){
+    return array(
+      array(
+        'title'           => 'Razorpay Settings',
+        'fields'          => array(
+          array(
+            'name'        => 'gf_razorpay_company_name',
+            'label'       => esc_html__('Comapny Name', $this->_slug),
+            'type'        => 'text',
+            'class'       => 'medium',
+          ),
+          array(
+            'name'        => self::GF_RAZORPAY_KEY,
+            'label'       => esc_html__('Test Razorpay Key', $this->_slug),
+            'type'        => 'text',
+            'class'       => 'medium',
+          ),
+          array(
+            'name'        => self::GF_RAZORPAY_SECRET,
+            'label'       => esc_html__('Test Razorpay Secret', $this->_slug),
+            'type'        => 'text',
+            'class'       => 'medium',
+          )
+        ),
+      ),
+    );
+  }
+
+  public function feed_list_no_item_message() {
+		$settings = $this->get_plugin_settings();
+		if ( ! rgar( $settings, self::GF_RAZORPAY_KEY ) || ! rgar( $settings, self::GF_RAZORPAY_SECRET ) ) {
+			return sprintf( esc_html__( 'To get started, please configure your %Razorpay Settings%s!', $this->_slug ), '<a href="' . admin_url( 'admin.php?page=gf_settings&subview=' . $this->_slug ) . '">', '</a>' );
+		} else {
+			return parent::feed_list_no_item_message();
+		}
+  }
+
+  public function get_customer_fields($form, $feed, $entry) {
+    $fields = array();
+
+    $billing_fields = $this->billing_info_fields();
+
+    foreach ($billing_fields as $field) {
+      $field_id = $feed['meta']['billingInformation_' . $field['name']];
+
+      $value = $this->get_field_value($form, $entry, $field_id);
+
+      $fields[$field['name']] = $value;
+    }
+
+    return $fields;
+  }
+
+  public function redirect_url( $feed, $submission_data, $form, $entry ) {
+    //updating lead's payment_status to Processing
+		GFAPI::update_entry_property( $entry['id'], 'payment_status', 'Processing' );
+    
+    $this->generate_razorpay_order($entry, $form);
+  }
+
+  public function generate_razorpay_order($entry, $form) {
+    //gravity form method to get value of payment_amount key from entry
+    $paymentAmount = rgar($entry, 'payment_amount');
+
+    //It will be null first time in the entry
+    if (empty($paymentAmount) === true) {
+      $paymentAmount = GFCommon::get_order_total($form, $entry);
+      gform_update_meta($entry['id'], 'payment_amount', $paymentAmount);
+      $entry['payment_amount'] = $paymentAmount;
+    }
+
+    $key = $this->get_plugin_setting(self::GF_RAZORPAY_KEY);
+
+    $secret = $this->get_plugin_setting(self::GF_RAZORPAY_SECRET);
+
+    $api = new Api($key, $secret);
+
+    $data = array(
+      'receipt'         => $entry['id'],
+      'amount'          => $paymentAmount * 100,
+      'currency'        => $entry['currency'],
+      'payment_capture' => 1
+    );
+
+    $razorpayOrder = $api->order->create($data);
+
+    gform_update_meta($entry['id'], self::RAZORPAY_ORDER_ID, $razorpayOrder['id']);
+
+    $entry[self::RAZORPAY_ORDER_ID] = $razorpayOrder['id'];
+
+    GFAPI::update_entry($entry);
+
+    setcookie(
+      self::RAZORPAY_ORDER_ID,
+      $entry[self::RAZORPAY_ORDER_ID],
+      time() + self::COOKIE_DURATION,
+      COOKIEPATH,
+      COOKIE_DOMAIN,
+      false,
+      true
+    );
+
+    echo $this->generate_razorpay_form($entry, $form);
+  }
+
+  public function generate_razorpay_form($entry, $form) {
+    //updating lead's payment_status to Processing
+    GFAPI::update_entry_property( $entry['id'], 'payment_status', 'Processing' );
+
+    $feed = $this->get_payment_feed($entry, $form);
+
+    $customerFields = $this->get_customer_fields($form, $feed, $entry);
+
+    $key = $this->get_plugin_setting(self::GF_RAZORPAY_KEY);
+
+    $razorpayArgs = array(
+      'key'         => $key,
+      'name'        => $this->get_plugin_setting('gf_razorpay_company_name'),
+      'amount'      => $entry['payment_amount'] * 100,
+      'currency'    => $entry['currency'],
+      'description' => $form['description'],
+      'prefill'     => array(
+        'name'    => $customerFields[self::CUSTOMER_FIELDS_NAME],
+        'email'   => $customerFields[self::CUSTOMER_FIELDS_EMAIL],
+        'contact' => $customerFields[self::CUSTOMER_FIELDS_CONTACT],
+      ),
+      'notes'       => array(
+        'gravity_forms_order_id' => $entry['id']
+      ),
+      'order_id'    => $entry[self::RAZORPAY_ORDER_ID],
+    );
+
+    wp_enqueue_script(
+      'razorpay_script',
+      plugin_dir_url(__FILE__) . 'assets/js/script.js',
+      array('checkout')
+    );
+
+    wp_localize_script(
+      'razorpay_script',
+      'razorpay_script_vars',
+      array(
+        'data' => $razorpayArgs
+      )
+    );
+
+    wp_register_script(
+      'checkout',
+      'https://checkout.razorpay.com/v1/checkout.js',
+      null,
+      null
+    );
+
+    wp_enqueue_script('checkout');
+
+    $redirect_url = '?page=' . $this->_slug;
+
+    return $this->generate_order_form($redirect_url);
+  }
+
+  public function generate_order_form($redirect_url) {
+    $html = "<form id ='razorpayform' name='razorpayform' action='{$redirect_url}' method='POST'>
+      <input type='hidden' name='razorpay_payment_id' id='razorpay_payment_id'>
+      <input type='hidden' name='razorpay_signature'  id='razorpay_signature' >
+    </form>
+    <p id='msg-razorpay-success'  style='display:none'>
+      Please wait while we are processing your payment.
+    </p>
+    <p style='display:none'>
+      <button id='btn-razorpay'>Pay With Razorpay</button>
+      <button id='btn-razorpay-cancel' onclick='document.razorpayform.submit()'>Cancel</button>
+    </p>";
+    return $html;
+  }
+
+  public function callback() {
+    $razorpayOrderId = $_COOKIE[self::RAZORPAY_ORDER_ID];
+
+    $key = $this->get_plugin_setting(self::GF_RAZORPAY_KEY);
+
+    $secret = $this->get_plugin_setting(self::GF_RAZORPAY_SECRET);
+
+    $api = new Api($key, $secret);
+
+    try {
+      $order = $api->order->fetch($razorpayOrderId);
+    } catch (\Exception $e) {
+      $action = array(
+        'type'  => 'fail_payment',
+        'error' => $e->getMessage()
+      );
+
+      return $action;
+    }
+
+    $entryId = $order['receipt'];
+
+    $entry = GFAPI::get_entry($entryId);
+
+    $attributes = $this->get_callback_attributes();
+
+    $action = array(
+      'id'             => $attributes[self::RAZORPAY_PAYMENT_ID],
+      'type'           => 'fail_payment',
+      'transaction_id' => $attributes[self::RAZORPAY_PAYMENT_ID],
+      'amount'         => $entry['payment_amount'],
+      'entry_id'       => $entry['id'],
+      'error'          => 'Payment Failed',
+    );
+
+    $success = false;
+
+    if ((empty($entry) === false) and (empty($attributes[self::RAZORPAY_PAYMENT_ID]) === false) and (empty($attributes[self::RAZORPAY_SIGNATURE]) === false)
+    ) {
+      try {
+        $api->utility->verifyPaymentSignature($attributes);
+
+        $success = true;
+      } catch (SignatureVerificationError $e) {
+        $action['error'] = $e->getMessage();
+
+        return $action;
+      }
+    }
+
+    if ($success === true) {
+      $action['type'] = 'complete_payment';
+
+      $action['error'] = null;
+    }
+
+    return $action;
+  }
+
+  public function get_callback_attributes() {
+    return array(
+      self::RAZORPAY_ORDER_ID   => $_COOKIE[self::RAZORPAY_ORDER_ID],
+      self::RAZORPAY_PAYMENT_ID => sanitize_text_field(rgpost(self::RAZORPAY_PAYMENT_ID)),
+      self::RAZORPAY_SIGNATURE  => sanitize_text_field(rgpost(self::RAZORPAY_SIGNATURE)),
+    );
+  }
+
+  public function post_callback($callback_action, $callback_result) {
+    if ( is_wp_error( $callback_action ) || ! $callback_action ) {
+			return false;
+    }
+    
+    // Run the necessary Hooks
+    $entry          = GFAPI::get_entry( $callback_action['entry_id'] );
+		$feed           = $this->get_payment_feed( $entry );
+		$transaction_id = rgar( $callback_action, 'transaction_id' );
+		$amount         = rgar( $callback_action, 'amount' );
+		$id             = rgar( $callback_action, 'id' );
+		$status         = rgar( $callback_action, 'type' );
+    
+    if ( rgar( $callback_action, 'type' ) === 'complete_payment' ) {
+      do_action('gform_razorpay_complete_payment', $callback_action['transaction_id'], $callback_action['amount'], $entry, $feed);
+      if ( has_filter( 'gform_razorpay_complete_payment' ) ) {
+        $this->log_debug( __METHOD__ . '(): Executing functions hooked to gform_razorpay_complete_payment.' );
+      }
+      ?>
+      <div>
+        Your order has been successfully placed. Go back to the <a href="<?php echo home_url(); ?>">home page</a> and check your email.
+      </div>
+      <?php
+			$this->fulfill_order( $entry, $transaction_id, $amount, $feed );
+		} else {
+      GFAPI::update_entry_property( $entry['id'], 'payment_status', 'Failed' );
+      do_action('gform_razorpay_fail_payment', $entry, $feed);
+      if ( has_filter( 'gform_razorpay_fail_payment' ) ) {
+        $this->log_debug( __METHOD__ . '(): Executing functions hooked to gform_razorpay_fail_payment.' );
+      }
+    }
+  }
+
+  public function fulfill_order( &$entry, $transaction_id, $amount, $feed = null ) {
+
+		if ( ! $feed ) {
+			$feed = $this->get_payment_feed( $entry );
+		}
+
+		$form = GFFormsModel::get_form_meta( $entry['form_id'] );
+		if ( rgars( $feed, 'meta/delayPost' ) ) {
+			$this->log_debug( __METHOD__ . '(): Creating post.' );
+			$entry['post_id'] = GFFormsModel::create_post( $form, $entry );
+			$this->log_debug( __METHOD__ . '(): Post created.' );
+		}
+
+		if ( rgars( $feed, 'meta/delayNotification' ) ) {
+			//sending delayed notifications
+			$notifications = $this->get_notifications_to_send( $form, $feed );
+			GFCommon::send_notifications( $notifications, $form, $entry, true, 'form_submission' );
+		}
+
+		do_action( 'gform_razorpay_fulfillment', $entry, $feed, $transaction_id, $amount );
+		if ( has_filter( 'gform_razorpay_fulfillment' ) ) {
+			$this->log_debug( __METHOD__ . '(): Executing functions hooked to gform_razorpay_fulfillment.' );
+		}
+
+	}
+
+  public function is_callback_valid() {
+    // Will check if the return url is valid
+    if (rgget('page') !== $this->_slug) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public function billing_info_fields() {
+    $fields = array(
+      array('name' => self::CUSTOMER_FIELDS_NAME, 'label' => esc_html__('Name', 'gravityforms'), 'required' => false),
+      array('name' => self::CUSTOMER_FIELDS_EMAIL, 'label' => esc_html__('Email', 'gravityforms'), 'required' => false),
+      array('name' => self::CUSTOMER_FIELDS_CONTACT, 'label' => esc_html__('Phone', 'gravityforms'), 'required' => false),
+    );
+
+    return $fields;
+  }
+
+  public function init() {
+
+		add_filter( 'gform_notification_events', array( $this, 'notification_events' ), 10, 2 );
+		add_filter( 'gform_post_payment_action', array( $this, 'post_payment_action' ), 10, 2 );
+
+		// Supports frontend feeds.
+		$this->_supports_frontend_feeds = true;
+
+		parent::init();
+
+	}
+
+  // Custom Events
+  public function notification_events($notification_events, $form) {
+    $has_razorpay_feed = function_exists( 'gf_razorpay' ) ? gf_razorpay()->get_feeds( $form['id'] ) : false;
+
+    if ($has_razorpay_feed) {
+      $payment_events = array(
+        'complete_payment'          => __('Payment Completed', 'gravityforms'),
+        'refund_payment'            => __('Payment Refunded', 'gravityforms'),
+        'fail_payment'              => __('Payment Failed', 'gravityforms'),
+        'add_pending_payment'       => __('Payment Pending', 'gravityforms'),
+        'void_authorization'        => __('Authorization Voided', 'gravityforms'),
+        'create_subscription'       => __('Subscription Created', 'gravityforms'),
+        'cancel_subscription'       => __('Subscription Canceled', 'gravityforms'),
+        'expire_subscription'       => __('Subscription Expired', 'gravityforms'),
+        'add_subscription_payment'  => __('Subscription Payment Added', 'gravityforms'),
+        'fail_subscription_payment' => __('Subscription Payment Failed', 'gravityforms'),
+      );
+
+      return array_merge($notification_events, $payment_events);
+    }
+
+    return $notification_events;
+
+  }
+
+  public function post_payment_action($entry, $action) {
+    $form = GFAPI::get_form( $entry['form_id'] );
+    GFAPI::send_notifications( $form, $entry, rgar( $action, 'type' ) );
+  }
+}
+
+?>
---
+++ gf-razorpay.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Gravity Forms for Razorpay
+Plugin URI: http://www.gravityforms.com
+Description: Integrates Gravity Forms with Razorpay Payments, enabling end users to purchase goods and services through Gravity Forms.
+Version: 1.1
+Author: Saumya Majumder
+Author URI: https://www.isaumya.com
+Text Domain: razorpay-gravity-forms
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+This is the Razorpay payment gateway plugin for Gravity Forma. Allows you to accept credit cards, debit cards, netbanking and wallet with the gravity form plugin. It uses a seamles integration, allowing the customer to pay on your website without being redirected away from your website.
+
+*/
+
+define('GF_RAZORPAY_VERSION', '0.1');
+
+add_action('gform_loaded', array('GF_Razorpay_Bootstrap', 'load'), 5);
+
+class GF_Razorpay_Bootstrap {
+
+  public static function load() {
+
+    if (!method_exists('GFForms', 'include_payment_addon_framework')) {
+      return;
+    }
+
+    require_once('class-gf-razorpay.php');
+
+    GFAddOn::register('GFRazorpay');
+
+    // Register Indian Currency in the list of Gravity Forms Currency
+    add_filter('gform_currencies', function (array $currencies) {
+      $currencies['INR'] = array(
+        'name'               => __('Indian Rupee', 'gravityforms'),
+        'symbol_left'        => '&#8377;',
+        'symbol_right'       => '',
+        'symbol_padding'     => ' ',
+        'thousand_separator' => ',',
+        'decimal_separator'  => '.',
+        'decimals'           => 2
+      );
+
+      return $currencies;
+    });
+  }
+}
+
+function gf_razorpay() {
+  return GFRazorpay::get_instance();
+}
